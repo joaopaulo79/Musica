b2494dfbb3822d2228dcbc9b7bc3e5ffeb3ece71
João Paulo <joaopdejesus79@gmail.com>
2021-08-27 21:13:54 -0300

    Programa Finalizado!

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("  ");
+            Console.WriteLine("  ");
+            Console.WriteLine("EASY LOVE");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("ijiketenaide (easy!)");
+            Console.WriteLine("Easy, easy, every day (easy!)");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("dandan chikazuku mon janai"); 
+            Console.WriteLine("dou iu hannou kaitenai"); 
+            Console.WriteLine("ki ni nacchatte sashou ga nai"); 
+            Console.WriteLine("enerugii"); 
+            Console.WriteLine("onnanoko wa aa dakou da"); 
+            Console.WriteLine("mousou ganbou tada no kuusou"); 
+            Console.WriteLine("hora shiboridase"); 
+            Console.WriteLine("");
+            Console.WriteLine("nando datte makashitaii");
+            Console.WriteLine("datte kimi wa kawaii");
+            Console.WriteLine("hangeki wa shinaide yo");
+            Console.WriteLine("oshikitte kanzen shouri");
+            Console.WriteLine("");
+            Console.WriteLine("sukidakara sukina dake dainashi ni shitai no");
+            Console.WriteLine("Easy love (easy love!)");
+            Console.WriteLine("Easy love (night date!)");
+            Console.WriteLine("ne kimi ni dake");
+            Console.WriteLine("mada shiranai aoi haru mezameru kimochi wa");
+            Console.WriteLine("Easy love (easy love!)");
+            Console.WriteLine("Easy love (night date!)");
+            Console.WriteLine("Easy love (easy love!)");
+            Console.WriteLine("ai no katachi");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("ijiketenaide (easy!)");
+            Console.WriteLine("iji haranaide (easy!)");
+            Console.WriteLine("Easy, easy, every day (easy!)");
+            Console.WriteLine("sawagi makuri masho");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("  ");
+          // Console.WriteLine("");   
+
+
+        }
+  }
+}
